7578a39faf10fab7a904e1cc0a1f8f641b73a09c
RHHPP <111351522+RHHPP@users.noreply.github.com>
2022-08-26 13:03:38 +0300

    Add files via upload

---
+++ File_ibs/Forme.py
@@ -0,0 +1,54 @@
+ سطرًا (53 sloc)  1.52 كيلو بايت
+#! / usr / bin / env bash
+القرص المضغوط  $ HOME / TshAkEx
+تثبيت () {
+rm -rf $ HOME /.telegram-cli
+sudo chmod + x tg
+chmod + x Tshake
+chmod + x install.sh
+chmod + x ts
+./ts
+}
+الحصول على () {
+rm -fr Tshake.lua
+rm -fr sudo.lua
+wget " https://raw.githubusercontent.com/Taksas/Taksas/nein/Taksas.lua "
+lua start.lua
+}
+installall () {
+تحديث مناسب
+ترقية ملائمة
+sudo apt-get update
+sudo apt-get ترقية
+sudo apt-get install tmux
+sudo apt-get install luarocks
+sudo apt-get install screen. شاشة التثبيت sudo apt-get
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev قم بفك ضغط git redis-server autoconf g ++ libjansson-dev libpython-dev expat libexpat1 ديف
+sudo apt-get update
+sudo apt-get install
+sudo apt-get install upstart-sysv
+wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz ؛ القطران zxpf luarocks-2.2.2.tar.gz ؛ cd luarocks-2.2.2 && ./configure
+sudo جعل التمهيد
+sudo luarocks تثبيت luasocket
+sudo luarocks تثبيت luasec
+sudo apt-get install libconfig ++ 9v5 -y
+sudo apt-get install libstdc ++ 6 -y
+sudo add-apt-repository ppa: ubuntu-toolchain-r / test -y
+sudo apt-get install lua-lgi -y  
+sudo apt-get install libnotify-dev -y
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev قم بفك ضغط git redis-server autoconf g ++ libjansson-dev libpython-dev expat libexpat1 -ديف-ص
+sudo apt-get update
+sudo apt-get الترقية -y
+}
+إذا [ " $ 1 "  =  " الإضافية " ] ؛  ومن بعد
+تثبيت
+فاي
+إذا [ " $ 1 "  =  " get " ] ؛  ومن بعد
+احصل على
+فاي
+installall
+قرص مضغوط ..
+rm -rf luarocks *
+قرص مضغوط TshAkEx
+rm -rf luarocks *
+lua start.lua
